87177e563df0cdf790d61fd82c5b694ed72f3f27
Troy Chryssos <troychryssos@gmail.com>
2024-03-09 23:13:43 -0500

    cleanup

--- src/components/typography/Body.tsx
+++ src/components/typography/Body.tsx
@@ -6,7 +6,7 @@ export function Body({ children, className, bold, italic }: TypographyProps) {
   return (
     <p
       className={clsx(
-        'font-body text-base text-text',
+        'text-base text-text',
         bold && 'font-bold',
         italic && 'italic',
         className
--- src/components/typography/Title.tsx
+++ src/components/typography/Title.tsx
@@ -6,7 +6,7 @@ export function Title({ children, className, bold, italic }: TypographyProps) {
   return (
     <h2
       className={clsx(
-        'font-mono text-2xl text-text',
+        'text-2xl text-text',
         bold && 'font-bold',
         italic && 'italic',
         className
