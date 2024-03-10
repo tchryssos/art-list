043443f8a21214a4db984fc10ecff53cb552ea40
Troy Chryssos <troychryssos@gmail.com>
2024-03-09 23:05:25 -0500

    cleaning up app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps /* , AppContext */ } from 'next/app';
-import { useEffect, useState } from 'react';
 
-import { themes } from '~/constants/theme';
-import { AuthContext } from '~/logic/contexts/authContext';
-import { ColorMode } from '~/typings/colorMode';
-import { BreakpointsContext } from '~/logic/contexts/breakpointsContext';
+import { AuthContextProvider } from '~/logic/contexts/authContext';
 
 function Page({ Component, pageProps }: AppProps) {
-  const [colorMode] = useState<ColorMode>('standard');
-  const theme = themes[colorMode];
-
-  const [isAuthorized, setIsAuthorized] = useState(false);
-
-  useEffect(() => {
-    if (globalThis.document?.cookie) {
-      setIsAuthorized(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isAuthorized && !globalThis.document?.cookie) {
-      globalThis.document.cookie = 'isAuthorized=true';
-    }
-  }, [isAuthorized]);
-
   return (
-    <AuthContext.Provider value={{ isAuthorized, setIsAuthorized }}>
+    <AuthContextProvider>
       <Component {...pageProps} />
-    </AuthContext.Provider>
+    </AuthContextProvider>
   );
 }
 
