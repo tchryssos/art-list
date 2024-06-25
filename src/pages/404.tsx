fe6225a0d8aa15254307546de43887521192657f
Troy Chryssos <troychryssos@gmail.com>
2024-06-24 21:58:08 -0400

    no redirect on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,16 @@
-import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import { HOME_ROUTE } from '~/constants/routing';
 
 function FourOhFour() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(HOME_ROUTE);
+  }, [router]);
+
   return null;
 }
 
 export default FourOhFour;
-
-export const getStaticProps: GetStaticProps = async () => ({
-  redirect: {
-    destination: HOME_ROUTE,
-    permanent: false,
-  },
-});
