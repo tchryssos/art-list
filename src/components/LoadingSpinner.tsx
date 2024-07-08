b22cc280690394ffd4dc59f42f11f44deed6b6ca
Troy Chryssos <troychryssos@gmail.com>
2024-07-08 15:58:31 -0700

    too much prune

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,21 @@
+import { mdiLoading } from '@mdi/js';
+import Icon from '@mdi/react';
+import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface LoadingSpinnerProps {
+  className?: string;
+  size?: ComponentProps<typeof Icon>['size'];
+}
+
+export function LoadingSpinner({ className, size = 2 }: LoadingSpinnerProps) {
+  return (
+    <Icon
+      className={twMerge('text-text', className)}
+      path={mdiLoading}
+      size={size}
+      spin={1}
+      title="Loading"
+    />
+  );
+}
