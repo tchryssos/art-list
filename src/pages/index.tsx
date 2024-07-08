e365f2ea3ba336400819a5599b8805e7a7fc3f50
Troy Chryssos <troychryssos@gmail.com>
2024-07-08 14:20:20 -0700

    primitive pagination display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,13 +34,52 @@ interface PaginationProps {
 
 interface PaginationLinkProps {
   page: number;
+  currentPage: number;
 }
 
-function PaginationLink({ page }: PaginationLinkProps) {
+function PaginationLink({ page, currentPage }: PaginationLinkProps) {
   return (
-    <Link href={`${ART_LIST_ROUTE}?${PAGE_QUERY_PARAM}=${page}`}>
-      <Body>{page}</Body>
+    <Link
+      className="min-w-10 min-h-10 text-center"
+      href={`${ART_LIST_ROUTE}?${PAGE_QUERY_PARAM}=${page}`}
+    >
+      <Body className={currentPage === page ? 'underline' : ''}>{page}</Body>
     </Link>
+  );
+}
+
+const MAX_MIDDLE_LINKS = 3;
+
+interface MiddleLinkProps {
+  index: number;
+  currentPage: number;
+  pages: number;
+  lastPage: number;
+}
+
+function MiddleLink({ index, currentPage, pages, lastPage }: MiddleLinkProps) {
+  let showDots: null | 'start' | 'end' = null;
+
+  if (index === 0 && currentPage > 4) {
+    showDots = 'start';
+  }
+
+  if (index === MAX_MIDDLE_LINKS - 1 && currentPage <= pages - 4) {
+    showDots = 'end';
+  }
+
+  const page = currentPage + index + 1;
+
+  if (page >= lastPage) {
+    return null;
+  }
+
+  return (
+    <>
+      {showDots === 'start' && <Body>...</Body>}
+      <PaginationLink currentPage={currentPage} page={page} />
+      {showDots === 'end' && <Body>...</Body>}
+    </>
   );
 }
 
@@ -52,8 +91,17 @@ function Pagination({ count }: PaginationProps) {
 
   return (
     <div className="flex gap-2 mt-4 w-full justify-center">
-      <PaginationLink page={1} />
-      <PaginationLink page={lastPage} />
+      <PaginationLink currentPage={currentPage} page={1} />
+      {times(MAX_MIDDLE_LINKS, (i) => (
+        <MiddleLink
+          currentPage={currentPage}
+          index={i}
+          key={i}
+          lastPage={lastPage}
+          pages={pages}
+        />
+      ))}
+      <PaginationLink currentPage={currentPage} page={lastPage} />
     </div>
   );
 }
